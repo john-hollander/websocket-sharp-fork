52dbaf20e5c3df3a692dd78f2eb0585e82559537
John Hollander <john.hollander@stealthtrader.com>
2023-08-24 11:03:01 -0400

    Updated build setup.

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\devenv" "websocket-sharp.sln" /Rebuild Release
+xcopy /y/E/H/C/I Compiled\*.dll ..\themelia\Compiled\WebSocket
 xcopy /y/E/H/C/I Compiled\*.dll ..\antikythera\Compiled\WebSocket
 xcopy /y/E/H/C/I Compiled\*.dll ..\empennage\Compiled\WebSocket
 xcopy /y/E/H/C/I Compiled\*.dll ..\stealth-apis\Compiled\WebSocket
