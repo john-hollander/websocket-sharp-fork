04c7520433c76653db9846f14f7b3570ae7a22fb
John Hollander <john.hollander@stealthtrader.com>
2023-08-24 10:15:34 -0400

    Added build script.

---
+++ Build.bat
@@ -0,0 +1,11 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\devenv" "websocket-sharp.sln" /Rebuild Release
+xcopy /y/E/H/C/I Compiled\*.dll ..\antikythera\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\empennage\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\stealth-apis\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\stealth-brokers\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\stealth-clients\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\stealth-utilities\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\strategy-builder\Compiled\WebSocket
+xcopy /y/E/H/C/I Compiled\*.dll ..\mercury\Compiled\WebSocket
+
+PAUSE
